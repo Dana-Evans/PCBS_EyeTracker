238d508da05a0776389f75ed1f1148141d43cff4
Dana Evans--Ladon <dana.la@hotmail.fr>
2019-12-05 17:00:38 +0100

    Started to make the docstring for sphinx

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+"""
+The main program that is used to play the game.
+
+:author: `Dana Ladon <dana.ladon@ens.fr>`_
+
+:date:  2019, december
+"""
 import cv2
 import numpy as np
 import pygame
 
-HEAD_COLOR = (255,255,0)
-EYES_COLOR = ((0,255,255), (255,0,255))
+HEAD_COLOR = (255, 255, 0)
+EYES_COLOR = ((0, 255, 255), (255, 0, 255))
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (127, 127, 127)
@@ -13,60 +20,110 @@ BLUE = (0, 0, 255)
 
 
 def detect_eyes(img, classifier):
-    """ Function that detect the eyes, so only in the first half of the face and return the right vs left eye """
+    """
+    Function that detect the eyes, so only in the first half of the face and return the right vs left eye
+    :param img: The image where the eyes should be located
+    :type img: numpy.ndarray
+    :param classifier: The classifier used to detect the eyes
+    :type classifier: cv2.CascadeClassifier
+
+    :UC: None
+    """
     gray_frames = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     eyes = classifier.detectMultiScale(gray_frames, 1.3, 5)
-    height = np.size(img, 0) # get the width and height of the face image
-    width  = np.size(img, 1)
-    left_eye, right_eye = None, None # In case no eyes are detected...
-    for(x, y, w, h) in eyes :
-        if y > height/2: # pass if the eye is not on the top of the face
+    height = np.size(img, 0)  # get the width and height of the face image
+    width = np.size(img, 1)
+    left_eye, right_eye = None, None  # In case no eyes are detected...
+    for (x, y, w, h) in eyes:
+        if y > height / 2:  # pass if the eye is not on the top of the face
             pass
-        
-        eyecenter = x + w /2 # get the eye center
+
+        eyecenter = x + w / 2  # get the eye center
         if eyecenter < width * 0.5:
             left_eye = img[y:y + h, x:x + w]
-            # cv2.rectangle(img,(x, y), (x+w, y+h), EYES_COLOR[0], 2)
         else:
             right_eye = img[y:y + h, x:x + w]
-            # cv2.rectangle(img,(x, y), (x+w, y+h), EYES_COLOR[1], 2)
-        
+
     return left_eye, right_eye
 
 
 def detect_face(img, classifier):
-    """ Function that detect the biggest face on the image and returns it """
+    """
+    Function that detect the biggest face on the image and returns it
+
+    :param img: The image where the face should be located
+    :type img: numpy.ndarray
+    :param classifier: The classifier used to detect the face
+    :type classifier: cv2.CascadeClassifier
+
+    :UC: None
+    """
     gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     coords = classifier.detectMultiScale(gray_frame, 1.3, 5)
     if len(coords) == 0:
-        return None # No faces detected
+        return None  # No faces detected
     biggest = (0, 0, 0, 0)
     for i in coords:
         if i[3] > biggest[3]:
-            biggest = i # Largest width is chosen (Could have been width and height combined also)
-    x,y,w,h = biggest
+            biggest = i  # Largest width is chosen (Could have been width and height combined also)
+    x, y, w, h = biggest
     return img[y:y + h, x:x + w]
 
 
 def cut_eyebrows(img):
-    """ Remove the eyebrows from the eye """
+    """
+    Remove the eyebrows from the eye
+
+    :param img: One eye where you want to remove the eyebrow from the image
+    :type img: numpy.ndarray
+    """
     height, width = img.shape[:2]
     eyebrow_h = int(height / 4)
     return img[eyebrow_h:height, 0:width]
 
 
 def blob_process(img, detector, threshold):
-    """ Detect the blob of the eye """
+    """
+    Detect the blob of the eye
+    :param img: The eye image without eyebrows
+    :type img: numpy.ndarray
+    :param detector: The detector object used to find the blob
+    :type detector: cv2.SimpleBlobDetector
+    :param threshold: The threshold we use to darken or lighten the black and white image
+                      (a threshold of 10, would convert any pixel below 10 to  0 and above 10 to 255)
+    :type threshold: int
+
+    :CU: 0 <= threshold <= 255
+    """
+    assert 0 <= threshold <= 255, f"Threshold is too high({threshold}). It should be between 0 and 255 "
     gray_frame = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     retval, img = cv2.threshold(gray_frame, threshold, 255, cv2.THRESH_BINARY)
-    img = cv2.erode(img, None, iterations=2) #1
-    img = cv2.dilate(img, None, iterations=4) #2
-    img = cv2.medianBlur(img, 5) #3
+    img = cv2.erode(img, None, iterations=2)  # 1
+    img = cv2.dilate(img, None, iterations=4)  # 2
+    img = cv2.medianBlur(img, 5)  # 3
     keypoints = detector.detect(img)
     return keypoints
 
 
-def draw_cross(screen, centerx, centery, width, height, crossthickness = 5, color = BLUE):
+def draw_cross(screen, centerx, centery, width, height, crossthickness=5, color=BLUE):
+    """
+    A simple function to draw a cross on the screen
+
+    :param screen: The screen where we want to do the cross
+    :type screen: pygame.Surface
+    :param centerx: The x position of the center of the cross
+    :type centerx: int
+    :param centery: The y position of the center of the cross
+    :type centery: int
+    :param width: the width of the cross
+    :type width: int
+    :param height: the height of the cross
+    :type height: int
+    :param crossthickness: the thickness of the lines of the cross (which are here rectangles in fact)
+    :type crossthickness: int
+    :param color: A color tuple that contains 3 values between 0 and 255
+    :type color: tuple
+    """
     pygame.draw.rect(screen, color,
                      (centerx - crossthickness // 2, centery - height // 2, crossthickness, height))
 
@@ -74,11 +131,10 @@ def draw_cross(screen, centerx, centery, width, height, crossthickness = 5, colo
                      (centerx - width // 2, centery - crossthickness // 2, width, crossthickness))
 
 
-
-
 def main():
     pygame.init()
     screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
     screen.fill(WHITE)
     W, H = pygame.display.Info().current_w, pygame.display.Info().current_h
 
@@ -88,7 +144,7 @@ def main():
     # First need the classifiers
     face_cascade = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
     eye_cascade = cv2.CascadeClassifier('data/haarcascade_eye.xml')
-    
+
     # Blob detector
     detector_params = cv2.SimpleBlobDetector_Params()
     detector_params.filterByArea = True
@@ -96,7 +152,7 @@ def main():
     detector_params.filterByConvexity = False
     detector_params.filterByInertia = False
     detector = cv2.SimpleBlobDetector_create(detector_params)
-    
+
     cap = cv2.VideoCapture(0)
     cv2.namedWindow('jeu')
     cv2.createTrackbar('threshold', 'jeu', 0, 255, lambda x: 0)
@@ -105,7 +161,7 @@ def main():
     step = 0  # Initialization variable (where the user has to look or if the game starts)
     keys = ['middle', 'top', 'right', 'bottom', 'left']
     capture_position = [False, False]
-    
+
     while True:
         ret, frame = cap.read()
         face_frame = detect_face(frame, face_cascade)
@@ -117,7 +173,8 @@ def main():
             if left_eye is not None:
                 left_eye = cut_eyebrows(left_eye)
                 keypoints = blob_process(left_eye, detector, threshold)
-                cv2.drawKeypoints(left_eye, keypoints, left_eye, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+                cv2.drawKeypoints(left_eye, keypoints, left_eye, (0, 0, 255),
+                                  cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
                 if capture_position[0]:
                     keyname = keys[step - 1]
                     pos = eyes_position.get(keyname, [])
@@ -125,11 +182,11 @@ def main():
                     eyes_position[keyname] = pos
                     capture_position[0] = False
 
-
             if right_eye is not None:
                 right_eye = cut_eyebrows(right_eye)
                 keypoints = blob_process(right_eye, detector, threshold)
-                cv2.drawKeypoints(right_eye, keypoints, right_eye, (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+                cv2.drawKeypoints(right_eye, keypoints, right_eye, (0, 0, 255),
+                                  cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
                 if capture_position[1]:
                     keyname = keys[step - 1]
                     pos = eyes_position.get(keyname, [])
@@ -176,7 +233,6 @@ def main():
                 draw_cross(screen, W // 10, H // 2, 50, 50)
 
         pygame.display.flip()  # Updating screen
-
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
